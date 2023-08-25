8408bcc32644742e9fa0da53c366a4433485c5d0
AaronXu07 <aaronssupmail@gmail.com>
2023-08-25 16:37:54 -0400

    update for the obstacle challenge code

--- src/ObstacleChallenge.py
+++ src/ObstacleChallenge.py
@@ -17,7 +17,10 @@ picam2.configure("preview")
 picam2.start()
 
 lower_black = np.array([0,0,0])
-upper_black = np.array([100,255,30])
+upper_black = np.array([140,255,40])  #home threshold
+
+#upper_black = np.array([170,255,50]) #away
+#upper_black = np.array([110,255,30])
 
 derivative = -1
 sendnum = 1500
@@ -32,7 +35,7 @@ PINK = (121, 28, 227)
 NAVY = (68, 42, 32)
 PURPLE = (128, 0, 128)
 # Line thickness of 4 px
-thickness = 4
+thickness = 3
 
 prev_frame_time = 0
 new_frame_time = 0
@@ -74,18 +77,11 @@ while True:
     fps = str(fps)
     cv2.putText(im, fps, (8, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
     
-    Left_points = [(20,205), (210,205), (210,305), (20,305)] #left region of interest
-    Right_points = [(620,210), (415,210), (415,310), (620,310)] #right region of interest
+    Left_points = [(10,225), (185,225), (185,320), (10,320)] #left region of interest
+    Right_points = [(630,230), (450,230), (450,325), (630,325)] #right region of interest
     
-    Mid_points = [(160,180), (500, 180), (500, 405), (160, 405)]
-    Turn_points = [(200, 340), (450, 340), (450, 375), (200, 375)]
-    
-    
-    L_subimage = im[205:305, 20:210] #was x 15-245
-    R_subimage = im[210:310, 415:620] #was y 225-320 x 380:620
-    
-    M_subimage = im[180:405, 160:500]
-    turn_subimage = im[340:375, 200:450]
+    Mid_points = [(140,210), (515, 210), (515, 405), (140, 405)]
+    Turn_points = [(200, 360), (450, 360), (450, 390), (200, 390)]
     
     # Using cv2.line() method to draw a yellow line with thickness of 4 px
     im = cv2.line(im, Left_points[0], Left_points[1], YELLOW, thickness)
@@ -107,6 +103,13 @@ while True:
     im = cv2.line(im, Turn_points[1], Turn_points[2], YELLOW, thickness)
     im = cv2.line(im, Turn_points[2], Turn_points[3], YELLOW, thickness)
     im = cv2.line(im, Turn_points[3], Turn_points[0], YELLOW, thickness)
+    
+    L_subimage = im[225:320, 10:185] #was x 15-245
+    R_subimage = im[230:325, 450:630] #was y 225-320 x 380:620
+    
+    M_subimage = im[205:405, 140:515]
+    turn_subimage = im[360:390, 180:480]
+    
     
     L_imgHSV = cv2.cvtColor(L_subimage, cv2.COLOR_BGR2HSV)
     R_imgHSV = cv2.cvtColor(R_subimage, cv2.COLOR_BGR2HSV)
@@ -139,8 +142,7 @@ while True:
             
             left_lane_a = LeftMaxA
             #print("Left Lane Area = " + str(left_lane_a))
-            #print("Number of Left Contours found = " + str(len(L_contours)))
-    
+            #print("Number of Left Contours found = " + str(len(L_contours)))  
     
     #right countours
     R_contours = cv2.findContours(R_mask, 
@@ -177,7 +179,8 @@ while True:
         
     
     turn_imgHSV = cv2.cvtColor(turn_subimage, cv2.COLOR_BGR2HSV)
-    lower_blue = np.array([100, 150, 0], np.uint8)
+    #lower_blue = np.array([100, 150, 0], np.uint8) #away
+    lower_blue = np.array([100, 120, 0], np.uint8) #home
     upper_blue = np.array([140, 255, 255], np.uint8)
     maskb = cv2.inRange(turn_imgHSV, lower_blue, upper_blue)
     blue_contours = cv2.findContours(maskb, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2] # find blue contours
@@ -208,8 +211,8 @@ while True:
         blue_line_a = 0
      
     turn_imgHSV = cv2.cvtColor(turn_subimage, cv2.COLOR_BGR2HSV)
-    lower_orange = np.array([5, 50, 50], np.uint8)
-    upper_orange = np.array([15, 255, 255], np.uint8)
+    lower_orange = np.array([8, 75, 50], np.uint8) # was [5, 50, 50]
+    upper_orange = np.array([18, 255, 255], np.uint8)
     masko = cv2.inRange(turn_imgHSV, lower_orange, upper_orange)
     orange_contours = cv2.findContours(masko, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2] # find blue contours
     
@@ -243,7 +246,7 @@ while True:
     
     #lower mask (0-10)
     lower_red = np.array([0, 100, 22])
-    upper_red = np.array([4, 255, 255])
+    upper_red = np.array([3, 255, 255])
     mask0 = cv2.inRange(M_imgHSV, lower_red, upper_red)
     
     #upper mask (170-180)
@@ -258,7 +261,7 @@ while True:
     #lower_green = np.array([50,115,27])
     #upper_green = np.array([100,255,255])
     lower_green = np.array([50,125,17])
-    upper_green = np.array([80,255,255])
+    upper_green = np.array([90,255,255])
     mask = cv2.inRange(M_imgHSV, lower_green, upper_green)
     
     green_contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2] #find green contours
@@ -274,7 +277,7 @@ while True:
                 RedMaxA = area
                 RedMaxI = i 
         
-        if (RedMaxA > 150):
+        if (RedMaxA > 75):
             Red_cnt = red_contours[RedMaxI]
             area = cv2.contourArea(Red_cnt)
             #cv2.drawContours(M_subimage, red_contours, RedMaxI, (0, 255, 0), 2)
@@ -297,7 +300,7 @@ while True:
                 GreenMaxA = area
                 GreenMaxI = i 
         
-        if (GreenMaxA > 150):
+        if (GreenMaxA > 75):
             G_cnt = green_contours[GreenMaxI]
             area = cv2.contourArea(G_cnt)
             #cv2.drawContours(M_subimage, green_contours, GreenMaxI, (0, 255, 0), 2)
@@ -310,15 +313,15 @@ while True:
             #print("Green Pillar Area = " + str(green_pillar_area))        
             #print("Number of green contours found = " + str(len(red_contours)))
             
-    if (len(red_contours) == 0 or RedMaxA <= 150):
+    if (len(red_contours) == 0 or RedMaxA <= 80):
         red_pillar_area = 0
-    if (len(green_contours) == 0 or GreenMaxA <= 150):
+    if (len(green_contours) == 0 or GreenMaxA <= 80):
         green_pillar_area = 0
     
     cv2.imshow("Camera", im)
     
     
-    sendnum=1395 #starts moving the car forwards (home 1360) (mat 1375)
+    sendnum=1385 #starts moving the car forwards (home 1375) (mat 1385)
     sendnum = str(sendnum)
     ser.write((sendnum + "\n").encode('utf-8'))
     
@@ -359,8 +362,8 @@ while True:
         else:
             derivative = error-prev_error
         
-        kp = 0.006
-        kd = 0.003
+        kp = 0.0075
+        kd = 0.0035
         
         steering = int((kp * error) + (kd * derivative))
         
@@ -369,86 +372,160 @@ while True:
         elif (steering < -28):
             steering = -28
         
+        if (right_lane_a <= 80): #make sure steering is limited so it doesn't steer too much 
+            steering = 32
+        elif (left_lane_a <= 80):
+            steering = -34
+        
         sendnum = angle-steering # Greater Than 2100 = to left | Less than 2100 = to the right 
-        #print("angle is: ", sendnum)
-        sendnum = str(sendnum)
-        ser.write((sendnum + "\n").encode('utf-8'))
         
         prev_error = error
-    
-    #print(red_pillar_area)
         
-    if (red_pillar_area >= 200): #movement for detection of red pillar
-        #print("red pillar x: ", red_x)
-        if(green_pillar_area >= 200):
+    
+    if (red_pillar_area >= 150): #movement for detection of red pillar
+        #print("red pillar area: ", red_pillar_area)
+        if(green_pillar_area >= 150):
             if(red_y > green_y):
-                if(red_pillar_area <= 2000):
+                if(red_pillar_area <= 750):
                     sendnum = 2078
-                elif(red_pillar_area <=4500):
+                elif(red_pillar_area <=1500):
+                    sendnum = 2076
+                elif(red_pillar_area <=2000):
                     sendnum = 2074
+                elif(red_pillar_area <=3000):
+                    sendnum = 2070
+                elif(red_pillar_area <=4000):
+                    sendnum = 2066
                 else:
-                    sendnum = 2064    
-                #print("angle is: ", sendnum)
-                sendnum = str(sendnum)
-                ser.write((sendnum + "\n").encode('utf-8'))  
+                    sendnum = 2060      
         
         else:
-            if(red_pillar_area <= 2000):
+            if(red_pillar_area <= 750):
                 sendnum = 2078
-            elif(red_pillar_area <=4500):
+            elif(red_pillar_area <=1500):
+                sendnum = 2076
+            elif(red_pillar_area <=2000):
                 sendnum = 2074
-            else:
+            elif(red_pillar_area <=3000):
+                sendnum = 2070
+            elif(red_pillar_area <=4000):
                 sendnum = 2066
-            #print("angle is: ", sendnum)
-            sendnum = str(sendnum)
-            ser.write((sendnum + "\n").encode('utf-8'))
-    
-    
-    if (green_pillar_area >= 200): #movement for detection of green pillar
-        #print("red pillar x: ", red_x)       
-        if(red_pillar_area >= 200):
-            if(green_y > red_y):
-                if(green_pillar_area <= 2000):
-                    sendnum = 2118
-                elif(green_pillar_area <= 4500):
-                    sendnum = 2122
-                else:
-                    sendnum = 2130
-                #print("angle is: ", sendnum)
-                sendnum = str(sendnum)
-                ser.write((sendnum + "\n").encode('utf-8'))       
-        else:
-            if(green_pillar_area <= 2000):
-                sendnum = 2118
-            elif(green_pillar_area <= 4500):
-                sendnum = 2122
             else:
-                sendnum = 2130      
-            #print("angle is: ", sendnum)
-            sendnum = str(sendnum)
-            ser.write((sendnum + "\n").encode('utf-8'))
-           
-           
+                sendnum = 2060     
+    
+    
+    if (green_pillar_area >= 150): #movement for detection of green pillar
+        #print("green pillar area: ", green_pillar_area)       
+        if(red_pillar_area >= 150):
+            if(green_y > red_y):
+                if(green_pillar_area <= 750):
+                    sendnum = 2118
+                elif(green_pillar_area <= 1500):
+                    sendnum = 2120
+                elif(green_pillar_area <= 2000):
+                    sendnum = 2124
+                elif(green_pillar_area <= 3000):
+                    sendnum = 2128
+                elif(green_pillar_area <= 4000):
+                    sendnum = 2132
+                else:
+                    sendnum = 2138
+                
+        else:
+            if(green_pillar_area <= 750):
+                sendnum = 2118
+            elif(green_pillar_area <= 1500):
+                sendnum = 2120
+            elif(green_pillar_area <= 2000):
+                sendnum = 2124
+            elif(green_pillar_area <= 3000):
+                sendnum = 2128
+            elif(green_pillar_area <= 4000):
+                sendnum = 2132
+            else:
+                sendnum = 2138
+                
+                
     if (CounterClockwise == True and Blue_Seen): #if needs to turn sharp left
-        sendnum = 2138
-        #print("angle is: ", sendnum)
-        sendnum = str(sendnum)
-        ser.write((sendnum + "\n").encode('utf-8'))
-            
+        sendnum = 2136
+               
     if (Clockwise == True and Orange_Seen): #if needs to turn sharp right  
         sendnum= 2058
-        #print("angle is: ", sendnum)
-        sendnum = str(sendnum)
-        ser.write((sendnum + "\n").encode('utf-8'))
+        
+    #print("angle is: ", sendnum)
+    sendnum = str(sendnum)
+    ser.write((sendnum + "\n").encode('utf-8'))
+           
             
-            
+    #ENDING SECTION
     if (count == 12): #end when see the upcoming line on ground
+        FinalFrame+=1
         
         if(CounterClockwise == True):
-            final_subimage = im[190:230, 210:400]        
+            
+            #final_subimage = im[195:230, 210:390]
+            final_subimage = im[220:265, 190:350]
             final_imgHSV = cv2.cvtColor(final_subimage, cv2.COLOR_BGR2HSV)
-            lower_blue = np.array([0, 0, 0])
-            upper_blue = np.array([180, 125, 70])
+            
+            
+            lower_blue = np.array([90, 70, 55], np.uint8) # was 100 (away)
+            upper_blue = np.array([140, 255, 255], np.uint8)
+            
+            maskf = cv2.inRange(final_imgHSV, lower_blue, upper_blue)
+            final_contours = cv2.findContours(maskf, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2] # find blue contours
+            
+            FMaxA = 0
+            FMaxI = 0
+            if (len(final_contours) > 0):
+                for i in range(len(final_contours)):
+                    F_cnt = final_contours[i]
+                    area = cv2.contourArea(F_cnt)
+                    if(area > FMaxA):
+                        FMaxA = area
+                        FMaxI = i 
+                
+                if (FMaxA > 80):
+                    F_cnt = final_contours[FMaxI]
+                    area = cv2.contourArea(F_cnt)
+                    #cv2.drawContours(final_subimage, final_contours, FMaxI, (0, 255, 0), 2)
+                    approx=cv2.approxPolyDP(F_cnt, 0.01*cv2.arcLength(F_cnt,True),True)
+                    x,y,w,h=cv2.boundingRect(approx)
+                    cv2.rectangle(final_subimage,(x,y),(x+w,y+h),(255, 255, 255),2)   
+                    final_line_a = FMaxA
+
+                    #print("Final Line Area = " + str(final_line_a))        
+                    #print("Number of final contours found = " + str(len(final_contours)))
+                        
+            if (len(final_contours) == 0 or FMaxA <= 85):
+                final_line_a = 0
+            
+            if(final_line_a > 85):
+                sendnum = str(1500)#stops car
+                ser.write((sendnum + "\n").encode('utf-8'))
+                sendnum = str(2098)
+                ser.write((sendnum + "\n").encode('utf-8'))
+                break #breaks out of program
+            
+            elif(FinalFrame > 320):
+                sendnum = str(1500)#stops car
+                ser.write((sendnum + "\n").encode('utf-8'))
+                sendnum = str(2098)
+                ser.write((sendnum + "\n").encode('utf-8'))
+                break #breaks out of program
+            
+        if(Clockwise == True):
+            #print("ending... orange")
+            
+            final_subimage = im[210:250, 230:400]
+            final_imgHSV = cv2.cvtColor(final_subimage, cv2.COLOR_BGR2HSV)
+            
+            """
+            lower_orange = np.array([5, 50, 50], np.uint8) #home
+            upper_orange = np.array([20, 255, 255], np.uint8)
+            """
+            lower_orange = np.array([8, 15, 165]) #away #was 0 not 8
+            upper_orange = np.array([40, 255, 255])
+            
             maskf = cv2.inRange(final_imgHSV, lower_orange, upper_orange)
             final_contours = cv2.findContours(maskf, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2] # find blue contours
             
@@ -474,57 +551,23 @@ while True:
                     #print("Final Line Area = " + str(final_line_a))        
                     #print("Number of final contours found = " + str(len(final_contours)))
                         
-            if (len(final_contours) == 0 or FMaxA <= 70):
+            if (len(final_contours) == 0 or FMaxA <= 85):
                 final_line_a = 0
             
-            if(final_line_a >= 80):
-                sendnum = str(1500)#stops car
-                ser.write((sendnum + "\n").encode('utf-8'))
-                sendnum = str(2098)
-                ser.write((sendnum + "\n").encode('utf-8'))
-                break #breaks out of program
-                    
-            
-        if(Clockwise == True):
-            final_subimage = im[190:225, 210:440]
-            final_imgHSV = cv2.cvtColor(final_subimage, cv2.COLOR_BGR2HSV)
-            lower_orange = np.array([5, 50, 50], np.uint8)
-            upper_orange = np.array([20, 255, 255], np.uint8)
-            maskf = cv2.inRange(final_imgHSV, lower_orange, upper_orange)
-            final_contours = cv2.findContours(maskf, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2] # find blue contours
-            
-            FMaxA = 0
-            FMaxI = 0
-            if (len(final_contours) > 0):
-                for i in range(len(final_contours)):
-                    F_cnt = final_contours[i]
-                    area = cv2.contourArea(F_cnt)
-                    if(area > FMaxA):
-                        FMaxA = area
-                        FMaxI = i 
-                
-                if (FMaxA > 100):
-                    F_cnt = final_contours[FMaxI]
-                    area = cv2.contourArea(F_cnt)
-                    #cv2.drawContours(final_subimage, final_contours, FMaxI, (0, 255, 0), 2)
-                    approx=cv2.approxPolyDP(F_cnt, 0.01*cv2.arcLength(F_cnt,True),True)
-                    x,y,w,h=cv2.boundingRect(approx)
-                    cv2.rectangle(final_subimage,(x,y),(x+w,y+h),(255, 255, 255),2)   
-                    final_line_a = FMaxA
-
-                    #print("Final Line Area = " + str(final_line_a))        
-                    #print("Number of final contours found = " + str(len(final_contours)))
-                        
-            if (len(final_contours) == 0 or FMaxA <= 70):
-                final_line_a = 0
-            
-            if(final_line_a >= 150):
+            if(final_line_a >= 85):
                 sendnum = str(1500)#stops car
                 ser.write((sendnum + "\n").encode('utf-8'))
                 sendnum = str(2098)
                 ser.write((sendnum + "\n").encode('utf-8')) 
                 break #breaks out of program
             
+            elif(FinalFrame > 340):
+                sendnum = str(1500)#stops car
+                ser.write((sendnum + "\n").encode('utf-8'))
+                sendnum = str(2098)
+                ser.write((sendnum + "\n").encode('utf-8'))
+                break #breaks out of program
+    
     if cv2.waitKey(1)==ord('q'):#wait until key ‘q’ pressed
         sendnum = str(1500)#stops car
         ser.write((sendnum + "\n").encode('utf-8'))
@@ -532,3 +575,4 @@ while True:
         ser.write((sendnum + "\n").encode('utf-8'))
         break
 cv2.destroyAllWindows() 
+
