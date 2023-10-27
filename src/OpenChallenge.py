f5c11db833dcaf4fd4aa1ec1b36485af73a2e943
AaronXu07 <aaronssupmail@gmail.com>
2023-10-26 22:52:30 -0400

    Added Ratio Feature
    Ratio feature was added to see the ratio of the contour area to the surrounding contour rectangle area. This allows the code to avoid detecting the blue line as a wall.

--- src/OpenChallenge.py
+++ src/OpenChallenge.py
@@ -18,7 +18,7 @@ picam2.start()
  
 
 lower_black = np.array([0,0,0]) #lower threshold values for black contour detection
-upper_black = np.array([120,255,30])#away upper threshold values for black contour detection
+upper_black = np.array([135,255,50])#away upper threshold values for black contour detection
 #upper_black = np.array([150,255,53]) #was [140,255,53]
 
 derivative = -1 #derivative variable for smooth lane follow, will be used when the program is run
@@ -49,6 +49,19 @@ Orange_Seen = False #variable true after the orange line is seen (necessary for 
 blue_line_a = 0 #variable that tracks the blue line's area
 orange_line_a = 0 #variable that tracks the orange line's area
 
+after_turn = 0 #counts how much time passed after the turn
+
+left_x = 0
+left_w = 0
+
+right_x = 640
+
+surround_area_L = 0
+surround_area_R = 0
+
+ratio_L = 0
+ratio_R = 0
+    
 if __name__ == '__main__':
     
     GPIO.setwarnings(False)#setup for the push buton to start the car
@@ -74,9 +87,11 @@ while True: #loop ends only when button is pressed which lets the program begin
 
 sleep(1)
 
+
 sendnum=1360#starts moving the car forwards (home 1360) (mat 1376) #was 90
 sendnum = str(sendnum) #converts the number to a string so that it can be sent
 ser.write((sendnum + "\n").encode('utf-8')) #sends the command to the arduino to be processed there
+
 
 while True: #main program loop
     
@@ -84,6 +99,7 @@ while True: #main program loop
     im= picam2.capture_array()
     
     #commented code below displays regions of interest borders and is for debugging and fixing errors in the algorithm
+    
     
     font = cv2.FONT_HERSHEY_SIMPLEX
     new_frame_time = time.time()
@@ -93,11 +109,20 @@ while True: #main program loop
     fps = str(fps)
     cv2.putText(im, fps, (8, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
     
-    Left_points = [(15,230), (240,230), (240,325), (15,325)] #left region of interest
-    Right_points = [(620,225), (380,225), (380,320), (620,320)] #right region of interest
     
-    Turn_points = [(200, 340), (450, 340), (450, 390), (200, 390)]
+    #subimages for the left and right regions of interest detecting left and right walls and lines on the ground
+    L_subimage = im[235:330, 20:250]#was 235:330, 20:260
+    R_subimage = im[235:330, 385:620]#was 235:330, 375:620
     
+    turn_subimage = im[365:410, 180:480]
+    
+    
+    Left_points = [(20,235), (250,235), (250,330), (20,330)] #left region of interest
+    Right_points = [(620,235), (385,235), (385,330), (620,330)] #right region of interest
+    
+    Turn_points = [(200, 365), (450, 365), (450, 410), (200, 410)]
+    
+    """
     # Using cv2.line() method to draw a yellow line with thickness of 4 px
     im = cv2.line(im, Left_points[0], Left_points[1], YELLOW, thickness)
     im = cv2.line(im, Left_points[1], Left_points[2], YELLOW, thickness)
@@ -113,13 +138,7 @@ while True: #main program loop
     im = cv2.line(im, Turn_points[1], Turn_points[2], YELLOW, thickness)
     im = cv2.line(im, Turn_points[2], Turn_points[3], YELLOW, thickness)
     im = cv2.line(im, Turn_points[3], Turn_points[0], YELLOW, thickness)
-    
-
-    #subimages for the left and right regions of interest detecting left and right walls and lines on the ground
-    L_subimage = im[230:325, 20:250] 
-    R_subimage = im[225:330, 375:620]
-    
-    turn_subimage = im[350:400, 180:480]
+    """
     
     #converts the images into HSV colour format to setup conour detection
     L_imgHSV = cv2.cvtColor(L_subimage, cv2.COLOR_BGR2HSV)
@@ -155,6 +174,9 @@ while True: #main program loop
             x,y,w,h=cv2.boundingRect(approx)
             cv2.rectangle(L_subimage,(x,y),(x+w,y+h),(0,0,255),2)
             
+            surround_area_L = w*h
+            left_x = x
+            left_w = w
             left_lane_a = LeftMaxA #assigns the maximum left contour area to left_lane_a
   
     
@@ -184,6 +206,8 @@ while True: #main program loop
             x,y,w,h=cv2.boundingRect(approx)
             cv2.rectangle(R_subimage,(x,y),(x+w,y+h),(0,0,255),2)
             
+            surround_area_R = w*h
+            right_x = x
             right_lane_a = RightMaxA #assigns the largest right contour found to right_lane_a
                     
     
@@ -256,6 +280,7 @@ while True: #main program loop
             x,y,w,h=cv2.boundingRect(approx)
             cv2.rectangle(turn_subimage,(x,y),(x+w,y+h),(255, 255, 255),2)
             
+            
             orange_line_a = OMaxA #orange_line_area is the detected orange line area
 
             #print("Orange Line Area = " + str(orange_line_a))        
@@ -268,6 +293,8 @@ while True: #main program loop
     #cv2.imshow("Camera", im)
     
     
+    after_turn+=1
+
     if(blue_line_a > 200): #tracking counterclockwise movement (detect blue line for turn) #was 300
         if(Clockwise == False and CounterClockwise == False): #only sets direction if no direction has been set, if blue line is seen first, it is counterclockwise
             CounterClockwise = True #sets CounterClowckwise variable to true so the program knows which direction the car is moving in
@@ -279,9 +306,10 @@ while True: #main program loop
         Orange_Seen = False #sets orange seen to false to stop the sharp turn for clockwise movement
     
     elif(CounterClockwise): #adding a turn count after it passes the blue line
-        if(TurnFrameCount > 2 and count < 12): #only happens if the turn has been over 2 frames to prevent bugs 
+        if(TurnFrameCount > 2 and count < 12 and after_turn > 30): #only happens if the turn has been over 2 frames to prevent bugs 
             TurnFrameCount=0 #resets the turnframecount variable
             count+=1
+            after_turn = 0
             print(count)
             
     if(orange_line_a > 200):#tracking clockwise movement (detect orange line for turn) #was 300
@@ -295,10 +323,25 @@ while True: #main program loop
         Blue_Seen = False
     
     elif(Clockwise):  #adding a turn count after it passes the orange line
-        if(TurnFrameCount > 2 and count < 12): #only counts a turn if the turn has been over 2 frames to prevent bugs
+        if(TurnFrameCount > 2 and count < 12 and after_turn > 30): #only counts a turn if the turn has been over 2 frames to prevent bugs
             TurnFrameCount=0#resets the turnframecount variable
             count+=1
+            after_turn = 0
             print(count)
+     
+    
+    if(left_lane_a > 0 and right_lane_a > 0):
+        ratio_L =(surround_area_L / left_lane_a)
+        ratio_R =(surround_area_R / right_lane_a)
+    else:
+        ratio_L = 0
+        ratio_R = 0
+        
+    if(ratio_L > 5):
+        left_lane_a = 0
+        
+    if(ratio_R > 5):
+        right_lane_a = 0
      
      
     #WALL FOLLOWING     
@@ -311,7 +354,7 @@ while True: #main program loop
         else:
             derivative = error-prev_error #the derivative is the difference between the previous error and the current error, the derivative value is used to smoothen the wall following
         
-        kp = 0.0055 #kp is the value we apply to the proportional error which converts the difference into an angle #0.005
+        kp = 0.006 #kp is the value we apply to the proportional error which converts the difference into an angle #0.005
         kd = 0.0040 #kd is the value we apply to the derivative error which helps the oscillation problem with only using proportional error
         
         steering = int((kp * error) + (kd * derivative)) #the steering angle is calculated by multiplying kp by the error and kd by the derivative error
@@ -325,40 +368,74 @@ while True: #main program loop
         sendnum = angle-steering # Greater Than 2100 = to left | Less than 2100 = to the right, the number sent is the straight angle minus the steering angle       
         
         prev_error = error #previous error is set at the end of the algorithm to be used in the next frame
-    
-    
-    #TURNING
-    elif (right_lane_a > 80 and left_lane_a <= 80): #if detect only right wall, it should do a large adjustment to the left
         
-        if(right_lane_a < 6000):
+        
+    if(Clockwise):                    
+                
+        if (right_lane_a <= 80 and left_lane_a > 80): #if detect only left wall, it should do a large adjustment to the right
+            
+            if(left_lane_a < 6000):
+                sendnum = 2074
+
+            elif(left_lane_a < 9000):
+                sendnum = 2072
+            
+            elif(left_lane_a >=9000):
+                sendnum = 2068
+                
+        elif (right_lane_a > 80 and left_lane_a <= 80): #if detect only right wall, it should do a large adjustment to the left
+        
+            if(right_lane_a < 6000):
+                sendnum = 2126
+
+            elif(right_lane_a < 9000):
+                sendnum = 2132
+            
+            elif(right_lane_a >= 9000):
+                sendnum = 2138      
+                
+    if(CounterClockwise):
+        if (right_lane_a > 80 and left_lane_a <= 80): #if detect only right wall, it should do a large adjustment to the left
+        
+            if(right_lane_a < 6000):
+                sendnum = 2126
+
+            elif(right_lane_a < 9000):
+                sendnum = 2132
+            
+            elif(right_lane_a >= 9000):
+                sendnum = 2138
+                
+                
+        elif (right_lane_a <= 80 and left_lane_a > 80): #if detect only left wall, it should do a large adjustment to the right
+            
+            if(left_lane_a < 6000):
+                sendnum = 2074
+
+            elif(left_lane_a < 9000):
+                sendnum = 2072
+            
+            elif(left_lane_a >=9000):
+                sendnum = 2068
+    
+    #print(ratio_L)
+
+    if (right_x < 40 and right_x < 230-(left_x+left_w)):
+        if(ratio_R < 5):
             sendnum = 2126
-
-        elif(right_lane_a < 9000):
-            sendnum = 2132
         
-        elif(right_lane_a >= 9000):
-            sendnum = 2138
-            
-            
-    elif (right_lane_a <= 80 and left_lane_a > 80): #if detect only left wall, it should do a large adjustment to the right
-        
-        if(left_lane_a < 6000):
+    if (230-(left_x+left_w) < 40 and 230-(left_x+left_w) < right_x):
+        if(ratio_L < 5):
             sendnum = 2074
-
-        elif(left_lane_a < 9000):
-            sendnum = 2072
-        
-        elif(left_lane_a >=9000):
-            sendnum = 2068
-
-
+      
+      
     if (CounterClockwise == True and Blue_Seen): #if needs to turn sharp left at a corner
-        sendnum = 2132
+        sendnum = 2130
                
     if (Clockwise == True and Orange_Seen): #if needs to turn sharp right at a corner
-        sendnum= 2074
+        sendnum= 2074 #was 2076
     
-    
+        
     #print("angle is: ", sendnum)
     sendnum = str(sendnum)#converts the number into a string so that it can be sent 
     ser.write((sendnum + "\n").encode('utf-8'))#sends the steering angle to the arduino
@@ -368,15 +445,15 @@ while True: #main program loop
     if (count == 12): #end when see the upcoming line on ground
         FinalFrame+=1 #adds 1 to how long it needs to wait until it ends
             
-        if(FinalFrame > 65):  #stops car after it has run 120 frames to make sure it ends at the correct place
+        if(FinalFrame > 80):  #stops car after it has run 120 frames to make sure it ends at the correct place
             sendnum = int(sendnum)
-            if(sendnum > 2092 and sendnum < 2104): #stops the car if it is straight enough
+            #if(sendnum > 2092 and sendnum < 2104): #stops the car if it is straight enough
                 #sleep(0.5)
-                sendnum = str(1500)#stops car
-                ser.write((sendnum + "\n").encode('utf-8'))
-                sendnum = str(2098)#straightens wheels
-                ser.write((sendnum + "\n").encode('utf-8'))
-                break #breaks out of program           
+            sendnum = str(1500)#stops car
+            ser.write((sendnum + "\n").encode('utf-8'))
+            sendnum = str(2098)#straightens wheels
+            ser.write((sendnum + "\n").encode('utf-8'))
+            break #breaks out of program           
     
     if cv2.waitKey(1)==ord('q'):#wait until key ‘q’ pressed
         sendnum = str(1500)#stops car
